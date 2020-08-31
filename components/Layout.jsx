c8a39c5e240ccddf54e2dedcb2696b893a64dd9d
andreas-groos <andreas.groos1@gmail.com>
2020-08-31 07:16:04 -0700

    Basic setup

---
+++ components/Layout.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+
+const scrollToTop = () => {
+  if (typeof window !== "undefined") {
+    console.log("SCROLL TO TOP");
+    try {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "auto",
+      });
+    } catch (error) {
+      // just a fallback for older browsers
+      window.scrollTo(0, 0);
+    }
+  }
+};
+
+function Layout({ children, fullScreen = true, className = "" }) {
+  useEffect(() => {
+    scrollToTop();
+  }, []);
+
+  return (
+    <div className="layout-wrapper">
+      <div className={`layout ${fullScreen ? "fullscreen" : ""} ${className}`}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
