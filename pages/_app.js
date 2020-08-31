fa0a7d14dbef1f507e679ebe9bcd0c19812cf711
andreas-groos <andreas.groos1@gmail.com>
2020-08-31 07:36:29 -0700

    prevent zoom on input focus

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/index.scss'
+import NextHead from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <NextHead>
+        < meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+      </NextHead>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
